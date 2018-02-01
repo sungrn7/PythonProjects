ea2ebe8cbb875cac5387820b81b4ddeb44355502
hubeen <hubeen__@naver.com>
2018-02-01 12:48:42 +0900

    register models

--- knualarm/knuapp/admin.py
+++ knualarm/knuapp/admin.py
@@ -10,7 +10,15 @@ from knuapp.models import bision_singwan
 from knuapp.models import school_singwan
 from knuapp.models import staff_singwan
 from knuapp.models import Account
-from knuapp.models import Announ
+from knuapp.models import Announ_kongju 
+from knuapp.models import Announ_brain 
+from knuapp.models import Announ_sabum 
+from knuapp.models import Announ_insa 
+#from knuapp.models import Announ_natural 
+from knuapp.models import Announ_indu 
+from knuapp.models import Announ_cnh 
+from knuapp.models import Announ_art 
+
 
 admin.site.register(dormi_choen)
 admin.site.register(staff_choen)
@@ -22,4 +30,11 @@ admin.site.register(bision_singwan)
 admin.site.register(school_singwan)
 admin.site.register(staff_singwan)
 admin.site.register(Account)
-admin.site.register(Announ)
+admin.site.register(Announ_kongju)
+admin.site.register(Announ_brain)
+admin.site.register(Announ_sabum)
+admin.site.register(Announ_insa)
+#admin.site.register(Announ_natural)
+admin.site.register(Announ_indu)
+admin.site.register(Announ_cnh)
+admin.site.register(Announ_art)
